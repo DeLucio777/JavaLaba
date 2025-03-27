2b416eaac26bc127f70ac3dab6da3c3308d0d844
De_Lucio <kvartyra125@gmail.com>
2025-03-27 12:04:06 +0300

    test1

---
+++ src/main/java/main/pack/Main.java
@@ -0,0 +1,19 @@
+package main.pack;
+
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello, Ilya");
+        Tester.printWelcomeMessage();
+        Tester tester = new Tester("Мишаня", "Жучара", 2, "Средний", 1000);
+        tester.displayInfo();
+        tester.displayInfo(tester.expirienceInYears);
+        tester.displayInfo(tester.englishLevel);
+        tester.displayInfo(tester.salary);
+    }
+}
+
+
+
+
---
+++ src/main/java/main/pack/Tester.java
@@ -0,0 +1,46 @@
+package main.pack;
+
+
+public class Tester {
+    public String name;
+    public String surname;
+    public int expirienceInYears;
+    public String englishLevel;
+    public double salary;
+
+
+    public Tester(String name, String surname) {
+        this(name, surname, 0, "Не", 0.0);
+    }
+
+    public Tester(String name, String surname, int expirienceInYears) {
+        this(name, surname, expirienceInYears, "Не", 0.0);
+    }
+
+    public Tester(String name, String surname, int expirienceInYears, String englishLevel, double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.expirienceInYears = expirienceInYears;
+        this.englishLevel = englishLevel;
+        this.salary = salary;
+    }
+
+    public void displayInfo() {
+        System.out.println("Имя: " + name + ", Фамилия: " + surname);
+    }
+
+    public void displayInfo(int years) {
+        System.out.println("Имя: " + name + ", Фамилия: " + surname + ", Опыт: " + years + " лет");
+    }
+
+    public void displayInfo(String level) {
+        System.out.println("Имя: " + name + ", Фамилия: " + surname + ", Уровень английского: " + level);
+    }
+    public void displayInfo(double salary) {
+        System.out.println("Имя: " + name + ", Фамилия: " + surname + ", Уровень английского: " + englishLevel + ", ЗП: " + salary);
+    }
+
+    public static void printWelcomeMessage() {
+        System.out.println("Добро пожаловать к тестерам.");
+    }
+}
